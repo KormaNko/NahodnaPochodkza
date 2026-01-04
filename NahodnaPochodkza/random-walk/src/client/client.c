43c3085af6d64b6d41655e74e0c4d5aa490459e4
KormaNko <150889306+KormaNko@users.noreply.github.com>
2026-01-04 14:56:23 +0100

    skusam menu v klientovi

--- NahodnaPochodkza/random-walk/src/client/client.c
+++ NahodnaPochodkza/random-walk/src/client/client.c
@@ -4,6 +4,34 @@
 #include <string.h>
 #include <unistd.h>
 #include "net.h"
+#include <pthread.h>
+
+typedef struct  {
+    int fd;
+}spolocneData;
+
+
+void * prijmac_sprav_od_servera(void * arg) {
+    spolocneData * sp = (spolocneData*)arg;
+    int cisloSocket = sp->fd;
+
+    char riadok[256];
+    while(1) {
+        int info = siet_precitaj_riadok(cisloSocket,riadok,sizeof(riadok));
+        if(info > 0){
+                    printf("Toto mi poslal server: %s",riadok);
+        }else if(info == 0) {
+            printf("Server ukoncil komunikaciu");
+            break;
+        }else {
+            printf("chyba");
+            break;
+        }
+    }
+    return NULL;
+}
+
+
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -13,12 +41,17 @@ int main(int argc, char **argv) {
 
     const char * host = argv[1];
     const char * port = argv[2];
-
     int pripojenie = siet_pripoj_sa_tcp(host,port);
     if(pripojenie < 0) {
         printf("Nepodarilo sa pripojit");
         return 1;
     }
+    
+    pthread_t komunikaciaServer;
+    spolocneData sp;
+    sp.fd = pripojenie;
+
+    pthread_create(&komunikaciaServer,NULL,prijmac_sprav_od_servera,&sp);
 
     const char * spravaPreServer = "HELLO\n";
 
@@ -28,14 +61,16 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char riadok[256];
-    int n = siet_precitaj_riadok(pripojenie,riadok,sizeof(riadok));
+    sleep(5);
+spravaPreServer = "QUIT\n";
 
-    if(n > 0) {
-        printf("Toto mi poslal server: %s",riadok);
-    }else {
-        printf("koniec");
+   odosielam = siet_posli_vsetko(pripojenie,spravaPreServer,strlen(spravaPreServer));
+    if (odosielam < 0) {
+        close(pripojenie);
+        return 1;
     }
+
+    pthread_join(komunikaciaServer,NULL);
     close(pripojenie);
     return 0;
 }
