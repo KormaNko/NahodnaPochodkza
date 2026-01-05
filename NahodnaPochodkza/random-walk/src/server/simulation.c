924961a82453d4737b9a34fe94e43e632b38b052
KormaNko <150889306+KormaNko@users.noreply.github.com>
2026-01-05 11:15:56 +0100

    detajly

--- NahodnaPochodkza/random-walk/src/server/simulation.c
+++ NahodnaPochodkza/random-walk/src/server/simulation.c
@@ -25,3 +25,63 @@ static void sim_get_center(const server_config *cfg, int *cx, int *cy) {
 *cy = cfg->vyska /2;
 }
 
+int sim_dojst_do_stredu_za_K(const server_config *cfg, int sx, int sy, int K) {
+    int cx,cy;
+    sim_get_center(cfg, &cx, &cy);
+    int x = sx, y = sy;
+    
+    if(x == cx && cy == y) {
+        return 1;
+    }
+    int i = 1;
+    while ( i <= K) {
+        sim_step(cfg,&x,&y);
+        if(x == cx && cy == y) {
+        return 1;
+    }
+    i++;
+    }
+    return 0;
+}
+
+unsigned long sim_kolko_krokov_kym_trafim(const server_config *cfg, int sx, int sy, unsigned long max_steps) {
+    int cx,cy;
+    sim_get_center(cfg, &cx, &cy);
+    int x = sx, y = sy;
+    if(x == cx && y == cy) {
+        return 0;
+    }
+    unsigned long i = 1;
+    while ( i < max_steps) {
+
+        sim_step(cfg,&x,&y);
+        if(x == cx && cy == y) {
+        return i;
+    }
+    i++;
+    }
+    return max_steps;
+}
+
+    sim_jedno_policko sim_simuluj_policko(const server_config *cfg, int sx, int sy, int K, int R, unsigned long max_steps) {
+        sim_jedno_policko policko;
+    
+        int trafil = 0;
+        unsigned long long suma_krokov = 0;
+
+
+        for (size_t i = 0; i < R; i++)
+        {
+            
+            trafil += sim_dojst_do_stredu_za_K(cfg,sx,sy,K);
+            suma_krokov += sim_kolko_krokov_kym_trafim(cfg,sx,sy,max_steps);
+
+        } 
+        policko.avg_steps_to_hit = (double)suma_krokov /R;
+        policko.p_hit_within_K =    (double)trafil /R;
+       
+        return policko;
+    }
+
+
+
